88e051b0279e904377b5be46d4c40ab228deab71
Mark Mossberg <mark.mossberg@gmail.com>
2013-08-26 20:50:02 -0400

    split into functions, checks usernames for validity

--- thrown-away.py
+++ thrown-away.py
@@ -7,19 +7,32 @@ import json
 import sys
 import urllib
 
-def main():
+### functions ##########
 
-	# primitive arg parsing
-	if len(sys.argv) < 2:
-		print "Usage: ./throw-away.py [username 1] [username 2] [etc]"
-		sys.exit()
+def die(message):
+	pre = "ERROR: "
+	print pre + message
+	sys.exit(1)
 
-	usernames = sys.argv[1:]
+def is_redditor(username):
+	req_url = "http://www.reddit.com/user/%s/about.json" % username
+	request = urllib.urlopen(req_url)
+	resp = request.read()
+	response = json.loads(resp)
+
+	if len(response) == 1:
+		return False
+	else:
+		return True
+
+def collect_karma(usernames):
 	pairs = {}
 	reddit_url = "http://www.reddit.com/user/%s/about.json"
 
-	# collect info
 	for name in usernames:
+		if not is_redditor(name):
+			die(name + " is not a redditor. Exiting...")
+
 		req_url = "http://www.reddit.com/user/%s/about.json" % name
 		request = urllib.urlopen(req_url)
 		resp = request.read()
@@ -28,17 +41,16 @@ def main():
 			link_karma = response['data']['link_karma']
 			comment_karma = response['data']['comment_karma']
 		except KeyError:
-			print "There was a mysterious error, try again!"
-			sys.exit()
+			die("There was a mysterious error, try again!")
 		pairs[name] = [link_karma, comment_karma]
 
-	# calculate & output 
+	return pairs
 
+def output(pairs):
 	total_link = 0
 	total_comment = 0
 
-	print """Karma Count (user, link, comment):
------------"""
+	print """Karma Count (user, link, comment):\n-----------"""
 	for pair in pairs:
 		output_str = "%s:\t%d\t%d" % (pair, pairs[pair][0], pairs[pair][1])
 		total_link += pairs[pair][0]
@@ -47,6 +59,19 @@ def main():
 
 	print "\nTotal Link Karma: %s" % total_link
 	print "Total Comment Karma: %s" % total_comment
+
+### main ##########
+
+def main():
+	# primitive arg parsing
+	if len(sys.argv) < 2:
+		print "Usage: ./throw-away.py [username 1] [username 2] [etc]"
+		sys.exit()
+
+	usernames = sys.argv[1:]
+	pairs = collect_karma(usernames)
+
+	output(pairs)
 
 if __name__ == '__main__':
 	main()
